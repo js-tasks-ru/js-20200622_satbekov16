c6ff7af00cb92ac618ff22a6a1a809c51fd858d4
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-06-26 20:58:43 +0600

    solution to sortStrings added

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,21 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
-
+export function sortStrings(arr, param = 'asc') {
+  let result = []
+  if(param === "asc") {
+    arr.sort((a, b) =>
+      a.localeCompare(b, undefined, {sensitivity: 'case', caseFirst: 'upper'}))
+    for(let i = 0; i < arr.length; i++) {
+      result[i] = arr[i];
+    }
+    return result;
+  } else if (typeof param === 'undefined' || param === "desc" ) {
+    arr.sort((a, b) =>
+      b.localeCompare(a, undefined, {sensitivity: 'case', caseFirst: 'upper'}));
+    for(let i = 0; i < arr.length; i++) {
+      result[i] = arr[i];
+    }
+    return result;
+  }
 }
