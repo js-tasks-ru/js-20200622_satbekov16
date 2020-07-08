d77238bf92a76190c5933f8d16f242104cd3143c
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-07-08 22:34:19 +0600

    added solution to Sortable-Table-v1

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -18,41 +18,41 @@ export default class ColumnChart {
 
   render() {
 
-      this.element = document.createElement("div");
+    this.element = document.createElement("div");
 
-      if(!this.data || this.data.length === 0) {
-        this.element.setAttribute("class", "column-chart_loading");
-        this.element.innerHTML = "<div><img src='charts-skeleton.svg'/></div>";
+    if(!this.data || this.data.length === 0) {
+      this.element.setAttribute("class", "column-chart_loading");
+      this.element.innerHTML = "<div><img src='charts-skeleton.svg'/></div>";
+    }
+
+    let output = "<div class=\"column-chart__title\">" + this.label;
+
+    if(this.link) {
+      output += "<a href=\"/sales\" class=\"column-chart__link\">" + this.link + "</a>"
+    }
+
+    output += "</div>" + "<div class=\"column-chart__container\">" + "<div class=\"column-chart__header\">" + this.value;
+
+    this.element.innerHTML = output;
+
+    if(this.data) {
+      const containerElement = this.element.querySelector(".column-chart__container");
+      const containerChartElement = document.createElement("div");
+      containerChartElement.setAttribute("class", "column-chart__chart");
+      containerElement.appendChild(containerChartElement);
+      const containerChartInnerElement = this.element.querySelector(".column-chart__chart");
+
+      for(let i = 0; i < this.data.length; i++) {
+        const dataElement = document.createElement("div");
+        const maxValue = Math.max(...this.data);
+        const scale = 50 / maxValue;
+        const toolTipValue = (this.data[i] / maxValue * 100).toFixed(0) + '%';
+        dataElement.setAttribute("style", "--value: " + Math.floor(this.data[i] * scale));
+        dataElement.setAttribute("data-tooltip", toolTipValue);
+        containerChartInnerElement.appendChild(dataElement);
       }
-
-      let output = "<div class=\"column-chart__title\">" + this.label;
-
-      if(this.link) {
-        output += "<a href=\"/sales\" class=\"column-chart__link\">" + this.link + "</a>"
-      }
-
-      output += "</div>" + "<div class=\"column-chart__container\">" + "<div class=\"column-chart__header\">" + this.value;
-
-      this.element.innerHTML = output;
-
-      if(this.data) {
-        const containerElement = this.element.querySelector(".column-chart__container");
-        const containerChartElement = document.createElement("div");
-        containerChartElement.setAttribute("class", "column-chart__chart");
-        containerElement.appendChild(containerChartElement);
-        const containerChartInnerElement = this.element.querySelector(".column-chart__chart");
-
-        for(let i = 0; i < this.data.length; i++) {
-          const dataElement = document.createElement("div");
-          const maxValue = Math.max(...this.data);
-          const scale = 50 / maxValue;
-          const toolTipValue = (this.data[i] / maxValue * 100).toFixed(0) + '%';
-          dataElement.setAttribute("style", "--value: " + Math.floor(this.data[i] * scale));
-          dataElement.setAttribute("data-tooltip", toolTipValue);
-          containerChartInnerElement.appendChild(dataElement);
-        }
-        this.element.innerHTML += "</div></div>";
-      }
+      this.element.innerHTML += "</div></div>";
+    }
 
   }
 
